eefc230ab99ee1a049d5875a336e20f70c24a609
Curtis <curtis_102@hotmail.com>
2023-04-07 21:34:38 -0700

    added theme

--- src/app/shared/directives/styled-button.directive.ts
+++ src/app/shared/directives/styled-button.directive.ts
@@ -1,10 +1,28 @@
-import { Directive, HostBinding } from '@angular/core';
+import {
+  Directive,
+  HostBinding,
+  OnInit,
+  Input,
+  Renderer2,
+  ElementRef,
+} from '@angular/core';
+
+type Mode = 'default' | 'danger' | 'modify';
 
 @Directive({
   selector: '[appStyledButton]',
 })
-export class StyledButtonDirective {
-  constructor() {}
+export class StyledButtonDirective implements OnInit {
+  @Input() mode: Mode = 'default';
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+  ngOnInit(): void {
+    this.addClass();
+  }
+
+  addClass() {
+    this.renderer.addClass(this.el.nativeElement, this.mode);
+  }
 
   @HostBinding('class.button-bg')
   get cssClasses() {
