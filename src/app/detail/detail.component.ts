311e106594d15482a79401a4ac53d5ec11fe0134
Curtis <curtis_102@hotmail.com>
2023-04-10 15:47:07 -0700

    added extra styling

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,6 +1,8 @@
 import { ActivatedRoute } from '@angular/router';
 import { Invoice } from 'src/app/utils/interfaces';
 import { Component, OnInit } from '@angular/core';
+import { BreakpointsService } from '../services/breakpoint.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-detail',
@@ -8,10 +10,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail.component.scss'],
 })
 export class DetailComponent implements OnInit {
+  $bp!: Observable<string>;
   invoice!: Invoice;
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private breakpointService: BreakpointsService
+  ) {}
 
   ngOnInit(): void {
     this.invoice = this.route.snapshot.data['invoice'];
+    this.$bp = this.breakpointService.breakpoint$;
   }
 }
